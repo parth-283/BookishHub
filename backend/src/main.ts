7422e7b4ca29951300cff5e98a2558491612e1ba
parth-283 <kp2832002@gmail.com>
2024-03-21 22:54:33 +0530

    Set dynamic port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
 
+  const port = process.env.PORT || 5000;
+
   const config = new DocumentBuilder()
     .setTitle('Bookishub API')
     .setDescription('The Bookishub API description')
@@ -16,6 +18,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(5000);
+  await app.listen(port);
 }
 bootstrap();
